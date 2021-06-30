099895f3eae1fcdc21edd51db7dbe81e283f67df
jpinto <jfpinto1989@gmail.com>
2021-06-30 17:28:53 -0400

    Add project property to Task object

--- src/task.js
+++ src/task.js
@@ -1,8 +1,9 @@
-function Task(name, desc, due, pri) {
+function Task(name, desc, due, pri, proj) {
   this.title = name
   this.description = desc
   this.dueDate = due
   this.priority = pri
+  this.project = proj
 }
 
 export { Task }
