e29512506b74f3fd3bb42cf68a918c56977afc2a
jpinto <jfpinto1989@gmail.com>
2021-07-02 17:29:33 -0400

    Basic layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import { User } from "./user.js"
+import "./style.css";
+import { User } from "./user.js";
+
+let topbar = document.createElement("div")
+document.body.appendChild(topbar)
+topbar.id = "topbar"
+topbar.innerHTML = "ToDo"
+
+let sidebar = document.createElement("div")
+document.body.appendChild(sidebar)
+sidebar.id = "sidebar"
+sidebar.innerHTML = "Sidebar"
+
+let main = document.createElement("div")
+document.body.appendChild(main)
+main.id = "main"
+main.innerHTML = "Main Content"
