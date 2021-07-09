5f3dcb2fdb8d416e50e684079447bdae79c53244
jpinto <jfpinto1989@gmail.com>
2021-07-09 16:40:57 -0400

    Add functions to load existing user

--- src/storage.js
+++ src/storage.js
@@ -9,7 +9,35 @@ const storage = (() => {
     localStorage.setItem('projects', projectsString)
   }
 
-  return { save }
+  const load = () => {
+    if ( userExists() ) {
+      let retrievedName = localStorage.getItem('name')
+      let retrievedTasks = localStorage.getItem("tasks")
+      let retrievedProjects = localStorage.getItem("projects")
+      
+      let parsedName = JSON.parse(retrievedName)
+      let parsedTasks = JSON.parse(retrievedTasks)
+      let parsedProjects = JSON.parse(retrievedProjects)
+
+      return { 
+        name: parsedName, 
+        tasks: parsedTasks, 
+        projects: parsedProjects 
+      }
+    } else {
+      return null
+    }
+  }
+
+  const userExists = () => {
+    if ('name' in localStorage) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  return { save, load }
 })()
 
 export { storage }
