093506fa2d61ec7ce89f30859c5705aed1121035
jpinto <jfpinto1989@gmail.com>
2021-07-12 12:16:43 -0400

    Initiate home page with user

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import "./style.css";
 import { dom } from "./dom.js";
 import { User } from "./user.js";
 
+const user = new User();
+user.attemptLoad();
+
 dom.pageInit();
+dom.printProjects(user.projArray)
