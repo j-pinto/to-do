aebecba8726d53b486cc1e64a6bf79dbd9678ce5
jpinto <jfpinto1989@gmail.com>
2021-07-20 20:10:02 -0400

    Input module with event listeners and handlers

--- src/index.js
+++ src/index.js
@@ -2,8 +2,40 @@ import "./style.css";
 import { dom } from "./dom.js";
 import { User } from "./user.js";
 
+const input = (function(){
+
+  const refreshProjectListeners = function(){
+    let projects = document.getElementsByClassName("projectListItem")
+    for (let i=0; i < projects.length; i++) {
+      projects[i].addEventListener('click', displaySelectedPoject)
+    };
+  
+    let upcoming = document.getElementById("upcoming")
+    upcoming.addEventListener('click', displayUpcoming)
+  }
+  
+  const displaySelectedPoject = function(){
+    let selectedProject = user.projArray.find( item => 
+      item.title == event.target.innerHTML
+      )
+  
+    dom.printTaskList(selectedProject.tasks)
+  }
+  
+  const displayUpcoming = function(){
+    dom.printUpcomingTasks(user.upcomingTasks)
+  }
+
+  return {
+    refreshProjectListeners
+  }
+})();
+
 const user = new User();
 user.attemptLoad();
+user.generateUpcomingTasks()
 
 dom.pageInit();
 dom.printProjectList(user.projArray)
+
+input.refreshProjectListeners()
